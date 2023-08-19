2ffe5e6c9fc826206b0628e921a54a198ef019b2
BigTrash <grant.jy.li@gmail.com>
2023-08-19 18:41:23 -0400

    ticker design completed

--- src/SearchResults/TickerComponent.jsx
+++ src/SearchResults/TickerComponent.jsx
@@ -10,21 +10,26 @@ function TickerComponent(props){
 
     let arrow
     (currentData.close > ticker.price)? arrow = upArrow : arrow = downArrow
+    
     return(
         <div className="TickerComponent">
-            <h1 id = "TickerName">{ticker.data.meta.symbol}</h1>
-            <img id = "arrow" src= {arrow}/>
-            <img id = "closeButton" src={closeIcon} onClick={()=>{exit(ticker.data.meta.symbol)}}/>
-
-            <div id = "leftData">
-                <h3>{"Share price: $" + parseFloat(ticker.price).toFixed(2)}</h3>
-                <h3>{"Latest high: $" + parseFloat(currentData.high).toFixed(2)}</h3>
-                <h3>{"Latest low: $" + parseFloat(currentData.low).toFixed(2)}</h3>
+            <div className = "topSection">
+                <h1 id = "TickerName">{ticker.data.meta.symbol}</h1>
+                <img id = "arrow" src= {arrow}/>
+                <img id = "closeButton" src={closeIcon} onClick={()=>{exit(ticker.data.meta.symbol)}}/>
             </div>
-            <div id = "rightData">
-                <h3>{"Latest open: $" + parseFloat(currentData.open).toFixed(2)}</h3>
-                <h3>{"Latest close: $" + parseFloat(currentData.close).toFixed(2)}</h3>
-                <h3>{"Stock volume: " + currentData.volume}</h3>
+
+            <div className= "dataInfo">
+                <div id = "leftData">
+                    <h3>{"Share price: $" + parseFloat(ticker.price).toFixed(2)}</h3>
+                    <h3>{"Latest high: $" + parseFloat(currentData.high).toFixed(2)}</h3>
+                    <h3>{"Latest low: $" + parseFloat(currentData.low).toFixed(2)}</h3>
+                </div>
+                <div id = "rightData">
+                    <h3>{"Latest open: $" + parseFloat(currentData.open).toFixed(2)}</h3>
+                    <h3>{"Latest close: $" + parseFloat(currentData.close).toFixed(2)}</h3>
+                    <h3>{"Stock volume: " + currentData.volume}</h3>
+                </div>
             </div>
         </div>
     )
