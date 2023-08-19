0ee50c26d28c94c79fe1ce2cc8c9cd09a5a5402b
BigTrash <grant.jy.li@gmail.com>
2023-08-19 02:26:01 -0400

    Arrows kind of working

--- src/SearchResults/TickerComponent.jsx
+++ src/SearchResults/TickerComponent.jsx
@@ -1,14 +1,19 @@
 import "./TickerComponent.css"
 import closeIcon from './images/close-icon.png'
+import upArrow from './images/up-arrow.png'
+import downArrow from './images/down-arrow.png'
 
 function TickerComponent(props){
     let ticker = props.ticker
     let exit = props.exit
     let currentData = ticker.data.values[0]
 
+    let arrow
+    (currentData.close > ticker.price)? arrow = upArrow : arrow = downArrow
     return(
         <div className="TickerComponent">
             <h1 id = "TickerName">{ticker.data.meta.symbol}</h1>
+            <img id = "arrow" src= {arrow}/>
             <img id = "closeButton" src={closeIcon} onClick={()=>{exit(ticker.data.meta.symbol)}}/>
 
             <div id = "leftData">
