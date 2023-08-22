75c872f96adad5af0c8e9960c4fdcd66164fcaf1
BigTrash <grant.jy.li@gmail.com>
2023-08-22 17:05:17 -0400

    login icon made
    pain is all I feel learning css

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import loginImage from './images/login.png'
 import './header.css'
 
 function Header() {
     return(
         <div className="HeaderMain">
             <h1 id="HeaderName">Quick Ticker Checker</h1>
+            <img src={loginImage} alt="" id ="login-icon"/>
         </div>
     )
 }
