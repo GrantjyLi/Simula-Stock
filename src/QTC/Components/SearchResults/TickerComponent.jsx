a7650f31b7cf46ca6510f3e3e08502922093ba7d
BigTrash <grant.jy.li@gmail.com>
2023-09-04 16:49:06 -0400

    purchase volume input done
    need to implement backend database saving now

--- src/QTC/Components/SearchResults/TickerComponent.jsx
+++ src/QTC/Components/SearchResults/TickerComponent.jsx
@@ -2,11 +2,14 @@ import "./TickerComponent.css"
 import closeIcon from './images/close-icon.png'
 import upArrow from './images/up-arrow.png'
 import downArrow from './images/down-arrow.png'
+import {useState} from 'react'
 
-function TickerComponent(props){
+export default function TickerComponent(props){
     let ticker = props.ticker
     let exit = props.exit
     let currentData = ticker.data.values[0]
+
+    const [volume, setVolume] = useState(0)
 
     let arrow
     (currentData.close > ticker.price)? arrow = upArrow : arrow = downArrow
@@ -19,18 +22,28 @@ function TickerComponent(props){
             </div>
 
             <div className= "dataInfo">
-                <div id = "leftData">
+                <div className = "data">
                     <h3>{"Share price: $" + parseFloat(ticker.price).toFixed(2)}</h3>
                     <h3>{"Latest high: $" + parseFloat(currentData.high).toFixed(2)}</h3>
                     <h3>{"Latest low: $" + parseFloat(currentData.low).toFixed(2)}</h3>
                 </div>
-                <div id = "rightData">
+                <div className = "data">
                     <h3>{"Latest open: $" + parseFloat(currentData.open).toFixed(2)}</h3>
                     <h3>{"Latest close: $" + parseFloat(currentData.close).toFixed(2)}</h3>
                     <h3>{"Stock volume: " + currentData.volume}</h3>
                 </div>
+
+            </div>
+            
+            <div id = "BuySell">
+                <h2 id = "purchase">Purchase volume: </h2>
+                <input type="number" id = "volumeInput"
+                    value = {volume}
+                    onChange={(e)=>{
+                        if (e.target.value <0) setVolume(0)
+                        else setVolume(e.target.value)
+                        }}/>
             </div>
         </div>
     )
 }
-export default TickerComponent;
